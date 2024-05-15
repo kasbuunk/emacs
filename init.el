b2d0cdb91e392aaa7f4beef973cd50335ad5bd93
Kas Buunk <kasbuunk@icloud.com>
2024-05-15 22:46:10 +0200

    feat: add forge

--- init.el
+++ init.el
@@ -75,7 +75,7 @@
  '(custom-safe-themes
    '("7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" default))
  '(package-selected-packages
-   '(magit evil-magit counsel-projectile projectile hydra general all-the-icons peach-melpa doom-themes helpful ivy-rich which-key rainbow-delimiters doom-modeline counsel swiper ivy command-log-mode go-mode vertico undo-fu gruvbox-theme evil-collection)))
+   '(forge magit evil-magit counsel-projectile projectile hydra general all-the-icons peach-melpa doom-themes helpful ivy-rich which-key rainbow-delimiters doom-modeline counsel swiper ivy command-log-mode go-mode vertico undo-fu gruvbox-theme evil-collection)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -208,3 +208,7 @@
 
 (use-package evil-magit
   :after magit)
+
+(use-package forge)
+
+(setq auth-sources '("~/.authinfo.gpg"))
